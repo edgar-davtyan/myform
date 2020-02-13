15c56b27ff8c02ced87907351af96de760d3152f
Edgar <edgar.davtyan.02@gmail.com>
2020-02-13 12:47:54 +0400

    First Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let formSwitch = document.querySelectorAll("[data-form]");
+
+for (let i = 0; i < formSwitch.length; i++) {
+    formSwitch[i].addEventListener("click", switchForm)
+}
+function switchForm(ev) {
+    let forms = document.querySelectorAll(".forms-inputs>div");
+    for (let i = 0; i < forms.length; i++) {
+        forms[i].classList.add("hidden")
+    }
+    let formClass = ev.target.getAttribute("[data-form]");
+    let form = document.querySelector("." + formClass);
+    if (form) {
+        form.classList.remove("hidden")
+    }
+}
