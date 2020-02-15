e22019f68a19abbedac697633468e809805e3b2f
Edgar <edgar.davtyan.02@gmail.com>
2020-02-15 17:23:44 +0400

    my form

--- web/index.js
+++ web/index.js
@@ -3,14 +3,17 @@ let formSwitch = document.querySelectorAll("[data-form]");
 for (let i = 0; i < formSwitch.length; i++) {
     formSwitch[i].addEventListener("click", switchForm)
 }
+
 function switchForm(ev) {
     let forms = document.querySelectorAll(".forms-inputs>div");
     for (let i = 0; i < forms.length; i++) {
         forms[i].classList.add("hidden")
     }
-    let formClass = ev.target.getAttribute("[data-form]");
+    let formClass = ev.target.getAttribute("data-form");
     let form = document.querySelector("." + formClass);
     if (form) {
         form.classList.remove("hidden")
     }
 }
+
+
