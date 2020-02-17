6d1b3a10770e19980edd22b701e7c67026a415e8
Edgar <edgar.davtyan.02@gmail.com>
2020-02-17 12:44:48 +0400

    add inp background

--- web/index.js
+++ web/index.js
@@ -1,4 +1,19 @@
 let formSwitch = document.querySelectorAll("[data-form]");
+let btn = document.querySelectorAll(".inp-sub");
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", inpRow)
+}
+
+function inpRow() {
+    let inputs = document.querySelectorAll(".inp");
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].value == 0) {
+            inputs[i].classList.add('inp-invalid')
+        }else {
+            inputs[i].classList.remove('inp-invalid')
+        }
+    }
+}
 
 for (let i = 0; i < formSwitch.length; i++) {
     formSwitch[i].addEventListener("click", switchForm)
