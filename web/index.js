fa0d095675a5bfe62364d96742b7bda5cd00f33e
Edgar <edgar.davtyan.02@gmail.com>
2020-02-19 14:39:07 +0400

    forms

--- web/index.js
+++ web/index.js
@@ -29,7 +29,7 @@ let validate = {
             return;
         }
         v = v[v.length - 1].codePointAt(0);
-        if (!((v >= 48 && v <= 57) || (v ==45))) {
+        if (!((v >= 48 && v <= 57) || (v == 45))) {
             this.value = this.value.slice(0, -1);
         }
     }
@@ -51,14 +51,27 @@ for (let i = 0; i < btn.length; i++) {
     btn[i].addEventListener("click", inpRow)
 }
 
-function inpRow() {
-    let inputs = document.querySelectorAll(".inp");
-    const p = document.querySelector(".inp-invalid-title");
+function inpRow(ev) {
+    let inputs = ev.target.closest('.form-item').querySelectorAll(".inp");
     for (let i = 0; i < inputs.length; i++) {
         if (inputs[i].value.length == 0) {
             inputs[i].classList.add('inp-invalid');
         } else {
-            inputs[i].classList.remove('inp-invalid')
+            inputs[i].classList.remove('inp-invalid');
+            let msg = inputs[i].closest('.input-field').querySelector('.input-message');
+            if (msg) {
+                msg.classList.remove("show")
+            }
+        }
+    }
+    let invalid = document.querySelector(".inp-invalid");
+    if (invalid) {
+        invalid.focus();
+        let msg = invalid.closest('.input-field').querySelector('.input-message');
+        if (msg) {
+            msg.classList.add("show")
+        } else {
+            switchForm(null, ev.target.closest('.form-item').nextElementSibling)
         }
     }
 }
@@ -67,14 +80,18 @@ for (let i = 0; i < formSwitch.length; i++) {
     formSwitch[i].addEventListener("click", switchForm)
 }
 
-function switchForm(ev) {
+function switchForm(ev, fObj) {
     let forms = document.querySelectorAll(".forms-inputs>div");
     for (let i = 0; i < forms.length; i++) {
         forms[i].classList.add("hidden")
     }
-    let formClass = ev.target.getAttribute("data-form");
-    let form = document.querySelector("." + formClass);
-    if (form) {
+    let form = null;
+    if (fObj) {
+        form = fObj
+    } else {
+        let formClass = ev.target.getAttribute("data-form");
+        form = document.querySelector("." + formClass);
+    }if (form) {
         form.classList.remove("hidden")
     }
 }
